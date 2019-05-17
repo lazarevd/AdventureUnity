dde92e546f44116650bc98cbe4c57521294c2bb9
lazarevd <lazarevd@gmail.com>
2019-05-18 01:52:17 +0300

    moves ok

--- UnityGit.sh
+++ UnityGit.sh
@@ -2,7 +2,7 @@ cd /d/Unity3d/NP
 git add -A
 echo -n "enter commit message: "
 read commit_name
-git commit -m $commit_name
+git commit -m "$commit_name"
 git remote add origin https://github.com/lazarevd/AdventureUnity.git
 git fetch
 git push -u origin master
